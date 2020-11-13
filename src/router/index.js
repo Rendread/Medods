7317ba96fd365785f6e827951aec23f476ea19ec
Дмитрий Поляков <rendreadcode@gmail.com>
2020-11-13 11:36:48 +0700

    init

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,20 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
+import Home from "../views/Home.vue";
 import Game from "../views/Game.vue";
 const routes = [
   {
     path: "/",
     name: "Home",
+    component: Home,
+  },
+  {
+    path: "/Medods/game",
+    name: "Game",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
     component: Game,
   },
 ];
